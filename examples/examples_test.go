d40d287b5d62e3594749bb46c13c0c0c1e253310
TooManySugar <toomanysugar.game@tutanota.com>
2023-01-17 14:26:54 +0300

    Added examples for random sequence and Log2Pow2n

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"testing"
+	"time"
+	"math/rand"
 
 	db2n "debruijnbtwon"
 )
@@ -66,6 +68,56 @@ func WriteB24SubsequencesExample(w io.Writer) {
 	}
 
 	db2n.FindDeBruijnSeqK2N(n, printN4SubsequenceVisitor)
+}
+
+// Part of math.rand Source
+type int63Source interface {
+	Int63() int64
+}
+
+type bufferedRandBitSource struct {
+	source int63Source
+	offset uint8
+	buf int64
+}
+
+func (s *bufferedRandBitSource) Bit() bool {
+	if s.offset >= 64 {
+		s.offset = 0
+		s.buf = s.source.Int63()
+	}
+	bit := (s.buf >> s.offset&1) == 1
+	s.offset++
+	return bit
+}
+
+func Get1RandomB26Example() uint64 {
+	src := rand.NewSource(time.Now().UnixNano())
+
+	s := &bufferedRandBitSource{
+		offset: 64,
+		source: src,
+	}
+	var res uint64
+	onRandFound := func(dbs uint64) bool {
+		res = dbs
+		return true
+	}
+
+	db2n.RandFindDeBruijnSeqK2N(6, s, onRandFound)
+
+	return res
+}
+
+func CreateLog2LookupTableExample(dbs uint64) []byte {
+	invert := make([]byte, 64)
+
+	for j := 0; j < 64; j++ {
+		v := uint64(1) << j * dbs >> 58
+		invert[v] = byte(j)
+	}
+
+	return invert
 }
 
 // Examples running tests
@@ -161,4 +213,37 @@ func TestWriteB24SubsequencesExample(t *testing.T) {
 
 	// utilUpdateWritingFuncOutput(f, refPath)
 	utilTestWritingFuncOutput(t, f, refPath)
+}
+
+func TestGet1RandomB26Example(t *testing.T) {
+	fmt.Printf("random B(2,6) sequence: 0x%016X\n", Get1RandomB26Example())
+}
+
+func TestCreateLog2LookupTableExample(t *testing.T) {
+	randomDBSeq := Get1RandomB26Example()
+	lookupT := CreateLog2LookupTableExample(randomDBSeq)
+
+	log2fromPow2n := func (pow2n uint64) uint8 {
+		return lookupT[pow2n * randomDBSeq >> 58]
+	}
+
+	for j := 0; j < 64; j++ {
+		if log2fromPow2n(uint64(1) << j) != byte(j) {
+			t.Fatal("invalid table generated")
+		}
+	}
+
+	fmt.Printf("magic:\n0x%016X\n", randomDBSeq)
+
+	fmt.Printf("lookup table:\n[64]uint8{\n\t%2d", lookupT[0])
+	for j := 1; j < 64; j++ {
+		fmt.Printf(",")
+		if j % 16 == 0 {
+			fmt.Printf("\n\t")
+		} else {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("%2d", lookupT[j])
+	}
+	fmt.Printf("}\n")
 }
